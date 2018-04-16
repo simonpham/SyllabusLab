b4eff3b77679a22e73b3046c90c0da0e1a9fee50
Simon Pham <simonpham.dn@gmail.com>
2018-04-16 09:55:15 +0700

    Add Post View activity

---
+++ Module2/src/main/java/com/hasbrain/areyouandroiddev/PostViewActivity.java
@@ -0,0 +1,33 @@
+package com.hasbrain.areyouandroiddev;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+/**
+ * Created by Simon Pham on 4/16/18.
+ * Email: simonpham.dn@gmail.com
+ */
+public class PostViewActivity extends AppCompatActivity {
+
+    private WebView vWeb = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_content);
+
+        initialize();
+
+        String url = getIntent().getStringExtra("Url");
+        loadUrl(url);
+    }
+
+    private void initialize() {
+        vWeb = findViewById(R.id.webView);
+    }
+
+    private void loadUrl(String url) {
+        if (vWeb != null) vWeb.loadUrl(url);
+    }
+}
