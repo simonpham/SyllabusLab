b1701859ecee6c558a45f8021008220d4dbc9ac9
Simon Pham <simonpham.dn@gmail.com>
2018-04-16 14:43:00 +0700

    Add list view footer

--- Module2/src/main/java/com/hasbrain/areyouandroiddev/PostListActivity.java
+++ Module2/src/main/java/com/hasbrain/areyouandroiddev/PostListActivity.java
@@ -1,8 +1,13 @@
 package com.hasbrain.areyouandroiddev;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -18,13 +23,20 @@ import java.util.List;
 
 public class PostListActivity extends AppCompatActivity {
 
+    public static final String REDDIT_PAGE_LINK = "https://www.reddit.com/r/androiddev/";
     public static final String DATA_JSON_FILE_NAME = "data.json";
     private FeedDataStore feedDataStore;
+
+    private TextView tvReddit;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResource());
+
+        initialize();
+
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.registerTypeAdapter(RedditPost.class, new RedditPostConverter());
         Gson gson = gsonBuilder.create();
@@ -49,11 +61,25 @@ public class PostListActivity extends AppCompatActivity {
                 }
             }
         }
+
+        tvReddit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PostViewActivity.class);
+                intent.putExtra("PostListActivity.POST_URL", REDDIT_PAGE_LINK);
+                getApplicationContext().startActivity(intent);
+            }
+        });
+    }
+
+    protected void initialize() {
+        list = findViewById(R.id.lvPosts);
+        View footerView = ((LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.footer_post_list, null, false);
+        list.addFooterView(footerView);
+        tvReddit = footerView.findViewById(R.id.tvRedditLink);
     }
 
     protected void displayPostList(List<RedditPost> postList) {
-        ListView list = findViewById(R.id.lvPosts);
-
         RedditPostAdapter adapter = new RedditPostAdapter(getApplicationContext(), postList);
 
         list.setAdapter(adapter);
