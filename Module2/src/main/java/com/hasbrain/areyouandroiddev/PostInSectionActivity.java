9354e4e4fb08626688bd59d44312d3a6c5040764
Simon Pham <simonpham.dn@gmail.com>
2018-04-23 12:17:59 +0700

    Add expandable post adapter

--- Module2/src/main/java/com/hasbrain/areyouandroiddev/PostInSectionActivity.java
+++ Module2/src/main/java/com/hasbrain/areyouandroiddev/PostInSectionActivity.java
@@ -1,7 +1,12 @@
 package com.hasbrain.areyouandroiddev;
 
+import android.widget.ExpandableListView;
+
+import com.hasbrain.areyouandroiddev.adapter.ExpandablePostAdapter;
 import com.hasbrain.areyouandroiddev.model.RedditPost;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -9,9 +14,44 @@ import java.util.List;
  */
 public class PostInSectionActivity extends PostListActivity {
 
+    List<String> listDataHeader;
+    HashMap<String, List<RedditPost>> listDataChild;
+    List<RedditPost> stickyPosts;
+    List<RedditPost> normalPosts;
+
+    ExpandableListView listExp;
+
     @Override
     protected void displayPostList(List<RedditPost> postList) {
-        //TODO: Display posts in sections.
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        stickyPosts = new ArrayList<>();
+        normalPosts = new ArrayList<>();
+
+        listDataHeader.add("Sticky posts");
+        listDataHeader.add("Normal posts");
+
+        int i;
+        for (i = 0; i < postList.size(); i++) {
+            if (postList.get(i).isStickyPost()) {
+                stickyPosts.add(postList.get(i));
+            } else {
+                normalPosts.add(postList.get(i));
+            }
+        }
+
+        listDataChild.put(listDataHeader.get(0), stickyPosts);
+        listDataChild.put(listDataHeader.get(1), normalPosts);
+
+        ExpandablePostAdapter adapter = new ExpandablePostAdapter(getApplicationContext(), listDataHeader, listDataChild);
+
+        listExp.setAdapter(adapter);
+
+
+//        adapter = new PostAdapter(this, postList);
+//        recyclerView.setAdapter(adapter);
+
     }
 
     @Override
---
+++ Module2/src/main/java/com/hasbrain/areyouandroiddev/adapter/ExpandablePostAdapter.java
@@ -0,0 +1,143 @@
+package com.hasbrain.areyouandroiddev.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import com.hasbrain.areyouandroiddev.model.RedditPost;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Simon Pham on 4/22/18.
+ * Email: simonpham.dn@gmail.com
+ */
+public class ExpandablePostAdapter extends BaseExpandableListAdapter {
+
+    private static final String REDDIT_PAGE_LINK = "https://www.reddit.com/r/androiddev/";
+
+    private final long ONE_MINUTE = 60;
+    private final long ONE_HOUR = 3600;
+    private final long ONE_DAY = 86400;
+    private final long ONE_WEEK = 604800;
+    private final long ONE_MONTH = 2629743;
+    private final long ONE_YEAR = 31556926;
+
+    private final int ITEM_POST = 0;
+    private final int ITEM_FOOTER = 1;
+
+    private Context context;
+
+    private final List<String> listDataHeader;
+    private final HashMap<String, List<RedditPost>> listChildData;
+
+
+    public ExpandablePostAdapter(Context context, List<String> listDataHeader, HashMap<String, List<RedditPost>> listChildData) {
+        this.context = context;
+        this.listDataHeader = listDataHeader;
+        this.listChildData = listChildData;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 2;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listChildData.get(listDataHeader.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listChildData.get(listDataHeader.get(groupPosition));
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listChildData.get(listDataHeader.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private String getDisplayTime(long time) {
+        String displayTime = getTimeAgo(time);
+        if (!(displayTime.startsWith("-") || displayTime.startsWith("0"))) {
+            return displayTime;
+        }
+
+        return new java.text.SimpleDateFormat("HH:mm:ss dd/MM/yyyy ").format(new java.util.Date(time * 1000));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private String getTimeAgo(long time) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        long elapsedTime = currentTime - time;
+
+        if (elapsedTime >= 0 && elapsedTime < ONE_MINUTE) {
+            return "A few seconds ago";
+        } else if (elapsedTime < ONE_HOUR) {
+            return toMinutes(time) + " minutes ago";
+        } else if (elapsedTime < ONE_DAY) {
+            return toHours(time) + " hours ago";
+        } else if (elapsedTime < ONE_MONTH) {
+            return toDays(time) + " days ago";
+        }
+
+        return new java.text.SimpleDateFormat("HH:mm:ss dd/MM/yyyy ").format(new java.util.Date(time * 1000));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private int toMinutes(long time) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        return Integer.parseInt(new java.text.SimpleDateFormat("m").format(new java.util.Date(currentTime * 1000)))
+                - Integer.parseInt(new java.text.SimpleDateFormat("m").format(new java.util.Date(time * 1000)));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private int toHours(long time) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        return Integer.parseInt(new java.text.SimpleDateFormat("H").format(new java.util.Date(currentTime * 1000)))
+                - Integer.parseInt(new java.text.SimpleDateFormat("H").format(new java.util.Date(time * 1000)));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private int toDays(long time) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        return Integer.parseInt(new java.text.SimpleDateFormat("D").format(new java.util.Date(currentTime * 1000)))
+                - Integer.parseInt(new java.text.SimpleDateFormat("D").format(new java.util.Date(time * 1000)));
+    }
+
+}
