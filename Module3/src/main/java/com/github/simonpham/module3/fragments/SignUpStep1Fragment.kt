316d605c0b026a3d275ea7a7a4f247f86432c413
Simon Pham <simonpham.dn@gmail.com>
2018-04-26 23:12:51 +0700

    Implement step 1 fragment's layout

--- Module3/src/main/java/com/github/simonpham/module3/fragments/SignUpStep1Fragment.kt
+++ Module3/src/main/java/com/github/simonpham/module3/fragments/SignUpStep1Fragment.kt
@@ -41,7 +41,7 @@ class SignUpStepOne : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_step_one, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up_step1, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
