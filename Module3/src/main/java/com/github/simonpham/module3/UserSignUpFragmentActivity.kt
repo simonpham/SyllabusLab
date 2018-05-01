c61286126c7bebee1f53f1af4c6e863784e4d2eb
Simon Pham <simonpham.dn@gmail.com>
2018-05-01 20:18:58 +0700

    Show step 1 fragment UI

--- Module3/src/main/java/com/github/simonpham/module3/UserSignUpFragmentActivity.kt
+++ Module3/src/main/java/com/github/simonpham/module3/UserSignUpFragmentActivity.kt
@@ -1,9 +1,9 @@
 package com.github.simonpham.module3
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 
-class UserSignUpFragmentActivity : AppCompatActivity() {
+class UserSignUpFragmentActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
